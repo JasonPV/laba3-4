64f96a01cfec405cacf21742418a6a513b1f953b
Евгений <38925021+JasonPV@users.noreply.github.com>
2021-04-13 21:53:47 +0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,282 @@
+import sys
+import matplotlib.pyplot as plt
+from methods import *
+from PyQt5.QtWidgets import *
+from PyQt5.QtGui import *
+
+
+class Example(QWidget):
+
+    def __init__(self):
+        super().__init__()
+
+        self.initUI()
+
+
+    def initUI(self):
+
+        lbl_get_best = QLabel('1) Get best params:', self)
+        lbl_get_best.move(1, 1)
+
+        lbl_get_examples = QLabel('2) Get examples:', self)
+        lbl_get_examples.move(1, 350)
+
+        lbl_get_cross = QLabel('3) Get cross validate:', self)
+        lbl_get_cross.move(1, 650)
+
+        lbl_sart_text = QLabel('4) Voting method:', self)
+        lbl_sart_text.move(1200, 1)
+        ###########################################################
+        #split data
+        lbl_split = QLabel(self)
+        lbl_split.setText('Enter the number of the test sample')
+        self.qle_1 = QLineEdit(self)
+        self.qle_1.move(100, 100)
+        lbl_split.move(60, 40)
+        btn_split = QPushButton('Ok', self)
+        btn_split.clicked.connect(self.on_click_split)
+        btn_split.move(120, 160)
+
+        #choose method
+        lbl_method = QLabel('Choose method', self)
+        self.combo_method = QComboBox(self)
+        self.combo_method.addItems(["histogram", "dft", "dct", "gradient", "scale"])
+        lbl_method.move(500, 40)
+        self.combo_method.move(507, 100)
+        btn_method = QPushButton('Choose', self)
+        btn_method.clicked.connect(self.on_click_method)
+        btn_method.move(510, 160)
+
+        #best param and score
+        lbl_score_on_test = QLabel('Score on test:', self)
+        lbl_best_param = QLabel('Best param:', self)
+        self.lbl_1 = QLabel('?', self)
+        self.lbl_2 = QLabel('?', self)
+        lbl_score_on_test.move(800, 40)
+        self.lbl_1.move(800, 70)
+        lbl_best_param.move(800, 120)
+        self.lbl_2.move(800, 150)
+        #########################################################
+        #choose method
+        lbl_method_2 = QLabel('Choose method' ,self)
+        self.combo_method_2 = QComboBox(self)
+        self.combo_method_2.addItems(["histogram", "dft", "dct", "gradient", "scale"])
+        btn_method_2 = QPushButton('Choose', self)
+        btn_method_2.clicked.connect(self.on_click_method_2)
+        lbl_method_2.move(100, 400)
+        self.combo_method_2.move(100, 460)
+        btn_method_2.move(100, 520)
+
+        #enter param
+        lbl_param = QLabel('Enter parametr', self)
+        self.qle_param = QLineEdit(self)
+        btn_param = QPushButton('Ok', self)
+        btn_param.clicked.connect(self.on_click_param)
+        lbl_param.move(500, 400)
+        self.qle_param.move(500, 460)
+        btn_param.move(500, 520)
+        self.lbl_pixmap_1 = QLabel(self)
+        self.lbl_pixmap_2 = QLabel(self)
+        self.lbl_pixmap_3 = QLabel(self)
+        self.lbl_pixmap_1.move(700, 400)
+        self.lbl_pixmap_2.move(850, 400)
+        self.lbl_pixmap_3.move(1000, 400)
+        self.lbl_plot_1 = QLabel(self)
+        self.lbl_plot_2 = QLabel(self)
+        self.lbl_plot_3 = QLabel(self)
+        self.lbl_plot_1.move(700, 530)
+        self.lbl_plot_2.move(850, 530)
+        self.lbl_plot_3.move(1000, 530)
+
+        ########################################################
+
+        #choose method
+        lbl_method_3 = QLabel('Choose method', self)
+        self.combo_method_3 = QComboBox(self)
+        self.combo_method_3.addItems(["histogram", "dft", "dct", "gradient", "scale"])
+        btn_method_3 = QPushButton('Choose', self)
+        btn_method_3.clicked.connect(self.on_click_method_3)
+        lbl_method_3.move(100, 700)
+        self.combo_method_3.move(100, 760)
+        btn_method_3.move(100, 820)
+
+        self.lbl_pixmap_cross = QLabel(self)
+        self.lbl_pixmap_cross.move(300, 700)
+
+        self.lbl_best_p = QLabel(self)
+        self.lbl_a = QLabel(self)
+        self.lbl_n = QLabel(self)
+        self.lbl_best_p1 = QLabel('Best param:', self)
+        self.lbl_a1 = QLabel('Accuracy:', self)
+        self.lbl_n1 = QLabel('Number of train:', self)
+        self.lbl_best_p1.move(650, 700)
+        self.lbl_a1.move(750, 700)
+        self.lbl_n1.move(850, 700)
+        self.lbl_best_p.move(650, 750)
+        self.lbl_a.move(750, 750)
+        self.lbl_n.move(850, 750)
+
+        ##################################################
+        #start
+        btn_start = QPushButton('Start', self)
+        btn_start.move(1250, 40)
+        btn_start.clicked.connect(self.on_click_start)
+        self.lbl_plot = QLabel(self)
+        self.lbl_plot.move(1250, 250)
+
+        self.lbl_best_par = QLabel(self)
+        self.lbl_ac = QLabel(self)
+        self.lbl_num = QLabel(self)
+        self.lbl_ac1 = QLabel('Accuracy:', self)
+        self.lbl_num1 = QLabel('Number of train:', self)
+        self.lbl_ac1.move(1400, 40)
+        self.lbl_num1.move(1500, 40)
+        self.lbl_num.move(1500, 90)
+        self.lbl_ac.move(1400, 90)
+
+        self.table = QTableWidget(self)
+        self.table.setColumnCount(7)
+        self.table.setRowCount(10)
+        col = 0
+        for i in ['train', 'get_histogram', 'get_dft', 'get_dct', 'get_gradient', 'get_scale', 'voting']:
+            cellinfo = QTableWidgetItem(i)
+            self.table.setItem(0, col, cellinfo)
+            col += 1
+        row = 1
+        for i in range(10):
+            cellinfo = QTableWidgetItem(str(i+1))
+            self.table.setItem(row, 0, cellinfo)
+            row += 1
+        self.table.move(1650, 40)
+
+        ########################################################
+        self.setGeometry(QDesktopWidget().availableGeometry())
+        self.setWindowTitle('Classifier')
+        self.show()
+
+    def on_click_split(self):
+        self.test = int(self.qle_1.text())
+
+    def on_click_method(self):
+        data = get_data()
+        data_train, data_test = get_split_data(data, self.test)
+        best_p, accuracy = get_best_params(data_train, data_test, eval('get_'+self.combo_method.currentText()))
+        self.lbl_1.setText(str(accuracy))
+        self.lbl_1.adjustSize()
+        self.lbl_2.setText(str(best_p))
+        self.lbl_2.adjustSize()
+
+    def on_click_method_2(self):
+        self.method_2 = eval('get_'+self.combo_method_2.currentText())
+
+    def on_click_param(self):
+        data = get_data()
+        samples, r = get_samples(data)
+        samples = samples[0]
+        images = random.choices(samples, k=3)
+        for i in range(3):
+            image = cv2.resize(images[i], (90, 90), interpolation=cv2.INTER_AREA)
+            cv2.imwrite('test.jpg', 255*image)
+            pixmap = QPixmap('test.jpg')
+            eval(f'self.lbl_pixmap_{i+1}').setPixmap(pixmap)
+            eval(f'self.lbl_pixmap_{i+1}').adjustSize()
+            if self.method_2 == get_histogram:
+                hist, bins = get_histogram(images[i], int(self.qle_param.text()))
+                hist = np.insert(hist, 0, 0.0)
+                fig = plt.figure(figsize=(1.1, 1.1))
+                ax = fig.add_subplot(111)
+                ax.plot(bins, hist)
+                plt.xticks(color='w')
+                plt.yticks(color='w')
+                plt.savefig('plot.png')
+                plt.close(fig)
+            elif self.method_2 == get_dct or self.method_2 == get_dft:
+                ex = self.method_2(images[i], int(self.qle_param.text()))
+                fig = plt.figure(figsize=(1.1, 1.1))
+                ax = fig.add_subplot(111)
+                ax.pcolormesh(range(ex.shape[0]), range(ex.shape[0]), np.flip(ex, 0))
+                plt.xticks(color='w')
+                plt.yticks(color='w')
+                plt.savefig('plot.png')
+                plt.close(fig)
+            elif self.method_2 == get_scale:
+                image = self.method_2(images[i], float(self.qle_param.text()))
+                cv2.imwrite('plot.png', 255 * image)
+            else:
+                ex = self.method_2(images[i], int(self.qle_param.text()))
+                fig = plt.figure(figsize=(1.1, 1.1))
+                ax = fig.add_subplot(111)
+                ax.plot(range(0, len(ex)), ex)
+                plt.xticks(color='w')
+                plt.yticks(color='w')
+                plt.savefig('plot.png')
+                plt.close(fig)
+
+            pixmap1 = QPixmap('plot.png')
+            eval(f'self.lbl_plot_{i+1}').setPixmap(pixmap1)
+            eval(f'self.lbl_plot_{i+1}').adjustSize()
+
+    def on_click_method_3(self):
+        data = get_data()
+        method = eval('get_'+self.combo_method_3.currentText())
+        ans, ps = get_cross(data, method)
+        fig, ax = plt.subplots(figsize=(3.2, 2.5))
+        # plt.figure(figsize=(2.5, 2.5))
+        # fig = plt.figure(figsize=(2.5, 2.5))
+        # ax = fig.add_subplot(111)
+        plt.plot(range(1, len(ans)+1), ans)
+        plt.grid(True)
+        plt.xlabel("Number of test")
+        plt.ylabel("Accuracy")
+        plt.savefig('plot1.png')
+        # plt.show()
+        plt.close()
+        pixmap2 = QPixmap('plot1.png')
+        self.lbl_pixmap_cross.setPixmap(pixmap2)
+        self.lbl_pixmap_cross.adjustSize()
+        self.lbl_best_p.setText(str(round(ps[ans.index(max(ans))], 2)))
+        self.lbl_best_p.adjustSize()
+        self.lbl_n.setText(str(ans.index(max(ans))+1))
+        self.lbl_n.adjustSize()
+        self.lbl_a.setText(str(max(ans)))
+        self.lbl_a.adjustSize()
+
+    def on_click_start(self):
+        data = get_data()
+        accuracy = []
+        ps = []
+        for i in range(9, 0, -1):
+            data_train, data_test = get_split_data(data, i)
+            r, p = get_vote(data_train, data_test)
+            a = accuracy_score(r, data_test[1])
+            p.append(a)
+            accuracy.append(a)
+            ps.append(p)
+
+        fig, ax = plt.subplots(figsize=(3.2, 2.5))
+        plt.plot(range(1, len(accuracy) + 1), accuracy)
+        plt.grid(True)
+        plt.xlabel("Number of test")
+        plt.ylabel("Accuracy")
+        plt.savefig('plot1.png')
+        plt.close()
+        pixmap2 = QPixmap('plot1.png')
+        self.lbl_plot.setPixmap(pixmap2)
+        self.lbl_plot.adjustSize()
+        self.lbl_ac.setText(str(max(accuracy)))
+        self.lbl_ac.adjustSize()
+        self.lbl_num.setText(str(accuracy.index(max(accuracy))+1))
+        self.lbl_num.adjustSize()
+
+
+        for i in range(len(ps)):
+            for j in range(6):
+                cellinfo = QTableWidgetItem(str(ps[i][j]))
+                self.table.setItem(i+1, j+1, cellinfo)
+
+
+if __name__ == '__main__':
+
+    app = QApplication(sys.argv)
+    ex = Example()
+    sys.exit(app.exec_())
---
+++ methods.py
@@ -0,0 +1,187 @@
+import cv2
+import numpy as np
+import random
+from sklearn.datasets import fetch_olivetti_faces
+from scipy.fftpack import dct
+from sklearn.metrics import accuracy_score
+
+
+def get_data():
+    data_images = fetch_olivetti_faces()
+    return data_images['images'], data_images['target']
+
+
+def get_histogram(image, param=30):
+    hist, bins = np.histogram(image, bins=np.linspace(0, 1, param))
+    return [hist, bins]
+
+
+def get_dft(image, mat_side=13):
+    f = np.fft.fft2(image)
+    f = f[0:mat_side, 0:mat_side]
+    return np.abs(f)
+
+
+def get_dct(image, mat_side=13):
+    c = dct(image, axis=1)
+    c = dct(c, axis=0)
+    c = c[0:mat_side, 0:mat_side]
+    return c
+
+
+def get_gradient(image, n=2):
+    shape = image.shape[0]
+    i, l = 0, 0
+    r = n
+    result = []
+
+    while r <= shape:
+        window = image[l:r, :]
+        result.append(np.sum(window))
+        i += 1
+        l = i * n
+        r = (i + 1) * n
+    result = np.array(result)
+    return result
+
+
+def get_scale(image, scale=0.35):
+    h = image.shape[0]
+    w = image.shape[1]
+    new_size = (int(h * scale), int(w * scale))
+    return cv2.resize(image, new_size, interpolation=cv2.INTER_AREA)
+
+
+def get_feature(data, method, parameter):
+    result = []
+    for element in data:
+        if method == get_histogram:
+            result.append(method(element, parameter)[0])
+        else:
+            result.append(method(element, parameter))
+    return result
+
+
+def distance(el1, el2):
+    return np.linalg.norm(np.array(el1) - np.array(el2))
+
+
+def classifier(train, data, method, param):
+    assert method in [get_scale, get_histogram, get_dct, get_dft, get_gradient]
+    feature_samples = get_feature(train[0], method, param)
+    feature_data = get_feature(data[0], method, param)
+    result = []
+    for element in feature_data:
+        mn = distance(element, feature_samples[0])
+        cl = 0
+        for i in range(1, len(feature_samples)):
+            dist = distance(element, feature_samples[i])
+            if dist < mn:
+                mn = dist
+                cl = i
+        # print(mn)
+        result.append(train[1][cl])
+    # accuracy = accuracy_score(result, data[1])
+    return result
+
+
+def get_best_params(train, test, method):
+    assert method in [get_scale, get_histogram, get_dct, get_dft, get_gradient]
+    if method == get_scale:
+        params = np.arange(0.1, 1, 0.05)
+    elif method == get_histogram:
+        params = np.arange(13, 300, 1)
+    elif method == get_gradient:
+        params = np.arange(3, int(test[0][0].shape[0]/2 - 1), 1)
+    else:
+        params = np.arange(3, min(test[0][0].shape), 1)
+
+    best_param = params[0]
+    results = classifier(train, test, method, best_param)
+    a = accuracy_score(results, test[1])
+    for p in params:
+        r = classifier(train, test, method, p)
+        a_b = accuracy_score(r, test[1])
+        if a_b >= a: #>=
+            a = a_b
+            best_param = p
+    return best_param, a
+
+
+def get_split_data(data, num=2):
+    # random.seed(41)
+    ran = [int(i) for i in range(10)]
+    random.shuffle(ran)
+    ran = ran[:num]
+    X = data[0]
+    y = data[1]
+    X_train = []
+    X_test = []
+    y_train = []
+    y_test = []
+    for i in range(0, 10):
+        if i not in ran:
+            for j in range(40):
+                X_train.append(X[i+10*j])
+                y_train.append(y[i+10*j])
+    for i in range(0, 10):
+        if i in ran:
+            for j in range(40):
+                X_test.append(X[i+10*j])
+                y_test.append(y[i+10*j])
+
+    data_train = [X_train, y_train]
+    data_test = [X_test, y_test]
+    # print(len(data_train[0]))
+    # print(len(data_test[0]))
+    return data_train, data_test
+
+
+def get_samples(data):
+    a = random.randint(0, 9)
+    # a = 8
+    arr1 = []
+    arr2 = []
+    for i in range(0, len(data[0]), 10):
+        arr1.append(data[0][i+a])
+        arr2.append(data[1][i+a])
+    samples = np.array([arr1, arr2], dtype=np.ndarray)
+
+    return samples, a
+
+
+def get_cross(data, method):
+    result = []
+    params = []
+    for i in range(9, 0, -1):
+        data_train, data_test = get_split_data(data, i)
+        p, a = get_best_params(data_train, data_test, method)
+        params.append(p)
+        result.append(a)
+    return result, params
+
+def get_vote(train, test):
+    methods = ['get_histogram', 'get_dft', 'get_dct', 'get_gradient', 'get_scale']
+    results = []
+    params = []
+    for m in methods:
+        p, a = get_best_params(train, test, eval(m))
+        results.append(classifier(train, test, eval(m), p))
+        params.append(a)
+    results = np.array(results)
+    res = results.transpose()
+    res = res.tolist()
+    res = list(map(lambda x: max(x, key=x.count), res))
+    return res, params
+
+
+if __name__ == '__main__':
+    data = get_data()
+    # samples = get_samples(data)
+    data_train, data_test = get_split_data(data, 0.2)
+
+    best_p, accuracy = get_best_params(data_test, data_train, get_scale)
+    print(best_p, accuracy)
+    res, ac = classifier(data_test, data_train, get_scale, best_p)
+    print(res)
+    print(ac)
